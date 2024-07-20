93985596be2a9971c14f4d808eaf820593cbed01
issacclicker <0712wn@gmail.com>
2024-07-20 22:55:12 +0900

    firstPerson Multiplay
    성공!!!!!~!~~~!

--- Assets/Scripts/TestingCollision.cs
+++ Assets/Scripts/TestingCollision.cs
@@ -1,16 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class TestingCollision : MonoBehaviour
+public class TestingCollision : NetworkBehaviour
 {
-    public GameObject gameObject;
-    private void OnTriggerEnter(Collider other) {
-        // if(other.CompareTag("Player")){
-        //     if(gameObject!=null){
-        //         gameObject.SetActive(false);
-        //     }
-        // }
-        GetComponent<MeshRenderer>().enabled = false;
+    private MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (IsServer)
+        {
+            // 서버에서만 상호작용 처리
+            if (other.CompareTag("Player"))
+            {
+                if (meshRenderer != null)
+                {
+                    meshRenderer.enabled = false;
+                }
+
+                // 클라이언트들에게 상태 변경 전파
+                HideObjectClientRpc();
+            }
+        }
+    }
+
+    [ClientRpc]
+    private void HideObjectClientRpc()
+    {
+        // 모든 클라이언트에서 MeshRenderer 비활성화
+        if (meshRenderer != null)
+        {
+            meshRenderer.enabled = false;
+        }
     }
 }
