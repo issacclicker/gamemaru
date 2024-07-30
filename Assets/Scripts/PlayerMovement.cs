a5e9986532b4e3580a74c9a1bb3f25e24e4699a3
Gyulyn Kim <90612408+tangerlyn@users.noreply.github.com>
2024-07-30 22:33:52 +0900

    점프 구현 완료
    콘솔 창 보면 플레이어가 걷고 있을 때(w키만 단독으로 눌렀을 때)는 지면에서 떨어져 있는 문제 확인. w+a 키 같이 누르면 지면에 붙어 점프 가능.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,6 @@ using UnityEngine.UI;
 
 public class PlayerMovement : NetworkBehaviour
 {
-
     //ThirdPerson Movement
     Animator _animator;
     Camera _camera;
@@ -25,6 +24,11 @@ public class PlayerMovement : NetworkBehaviour
     public bool run;
     public float smoothness = 10f;
 
+    public float jumpHeight = 1.5f;
+    public float gravity = -9.81f;
+    private Vector3 velocity;
+    private bool isGrounded;
+
     //Player State
     [SerializeField]
     public string playerState;
@@ -32,7 +36,6 @@ public class PlayerMovement : NetworkBehaviour
     private int huntFailures = 0;
     private bool isPenaltyActive = false;
     private int penaltyType = 0;
-    
 
     //Fox features(from fox1.cs)
     // public GameObject[] Bead;
@@ -45,36 +48,46 @@ public class PlayerMovement : NetworkBehaviour
     UIManager _uiManager; //UI관리
     HealthBar _healthBar; //여우 체력바
 
-
-    public GameObject animalModel;//이미호
-    public GameObject currentModel; 
+    public GameObject animalModel; //이미호
+    public GameObject currentModel;
     public GameObject originalModel;
     public bool isAwaken = false; //이미호 인지 아닌지
 
-
-
-    // // Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
-        _animator = this.GetComponent<Animator>();
+        _animator = GetComponent<Animator>();
+    if (_animator == null)
+    {     
+        Debug.LogError("Animator component is not found on the GameObject.");
+    }
+    else
+    {
+        Debug.Log("Animator component is found.");
+    }
         _camera = Camera.main;
-        _controller = this.GetComponent<CharacterController>();
-        
+        _controller = GetComponent<CharacterController>();
+
         //디버깅 용
-        if(IsHost){
+        if (IsHost)
+        {
             playerState = "Tiger";
-        }else{
+        }
+        else
+        {
             playerState = "Fox";
         }
 
-
-        if(!IsOwner){
+        if (!IsOwner)
+        {
             MainCamera.gameObject.SetActive(false);
             return;
-        }else{
+        }
+        else
+        {
             MainCamera.gameObject.SetActive(true);
         }
-        
+
         UIManagerObject = GameObject.Find("UIManager");
         _uiManager = UIManagerObject.GetComponent<UIManager>(); // UIManager 스크립트 가져오기
         _healthBar = UIManagerObject.GetComponent<HealthBar>(); // HealthBar 스크립트 가져오기
@@ -82,55 +95,47 @@ public class PlayerMovement : NetworkBehaviour
         UIManagerObject.GetComponent<UIManager>().playerState = playerState;
         _uiManager.UIEnable();
         _healthBar.IsGameStarted = true;
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // //For Network Owner
-        if(!IsOwner){
+        // For Network Owner
+        if (!IsOwner)
+        {
             return;
         }
 
         // PlayerState Separate
-        if(playerState=="Fox")
+        if (playerState == "Fox")
         {
             iDown = Input.GetButtonDown("Interaction");
-            if(!isAwaken)
+            if (!isAwaken)
             {
                 Interaction();
             }
-            
-            
         }
-        else if(playerState=="Tiger")
+        else if (playerState == "Tiger")
         {
-            //Tiger Penalty(from Tiger_Controller.cs)
+            // Tiger Penalty(from Tiger_Controller.cs)
             if (isPenaltyActive)
             {
                 ApplyPenalty();
             }
 
-            //Tiger UI update
+            // Tiger UI update
             _uiManager.UpdateHeartUI();
         }
-        else
-        {
 
-        }
-
-
-
-        //Thrid Person Movement
+        // Third Person Movement
         if (Input.GetKey(KeyCode.LeftAlt))
         {
-            toggleCameraRotation = true; // �ѷ����� Ȱ��ȭ
+            toggleCameraRotation = true; // 자유 시점 활성화
         }
         else
         {
-            toggleCameraRotation = false; // �ѷ����� ��Ȱ��ȭ
+            toggleCameraRotation = false; // 자유 시점 비활성화
         }
+
         if (Input.GetKey(KeyCode.LeftShift))
         {
             run = true;
@@ -139,28 +144,49 @@ public class PlayerMovement : NetworkBehaviour
         {
             run = false;
         }
+
+        isGrounded = _controller.isGrounded;
+
+        // 현재 플레이어가 지면에 닿아있지 않는 문제 확인
+        Debug.Log($"isGrounded: {isGrounded}");
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                Jump();
+            }
+        }
+
         InputMovement();
-        
+
+        // Apply gravity
+        velocity.y += gravity * Time.deltaTime;
+        _controller.Move(velocity * Time.deltaTime);
     }
 
     void LateUpdate()
     {
-
-        if(!IsOwner){
+        if (!IsOwner)
+        {
             return;
         }
 
-        if (toggleCameraRotation != true)
+        if (!toggleCameraRotation)
         {
             Vector3 playerRotate = Vector3.Scale(_camera.transform.forward, new Vector3(1, 0, 1));
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(playerRotate), Time.deltaTime * smoothness);
-
         }
     }
 
     void InputMovement()
     {
-        finalSpeed = (run) ? runspeed : speed;
+        finalSpeed = run ? runspeed : speed;
 
         Vector3 forword = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
@@ -169,12 +195,21 @@ public class PlayerMovement : NetworkBehaviour
 
         _controller.Move(moveDirection.normalized * finalSpeed * Time.deltaTime);
 
-        float percent = ((run) ? 1 : 0.5f) * moveDirection.magnitude;
-        _animator.SetFloat("Blend",  percent,0.1f,Time.deltaTime);
+        float percent = (run ? 1 : 0.5f) * moveDirection.magnitude;
+        _animator.SetFloat("Blend", percent, 0.1f, Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            _animator.SetTrigger("Jump");
+        }
     }
 
 
-    //Tiger Penalty
+    // Tiger Penalty
     void ApplyPenalty()
     {
         switch (penaltyType)
@@ -193,7 +228,6 @@ public class PlayerMovement : NetworkBehaviour
                 break;
         }
     }
-
 
     public void HuntFailure()
     {
