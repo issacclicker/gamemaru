a2683201ceb1080e1a7ae27b1dca6fbf65f0afa0
issacclicker <0712wn@gmail.com>
2024-08-01 17:06:48 +0900

    1

--- Assets/Scripts/Fox/Item.cs
+++ Assets/Scripts/Fox/Item.cs
@@ -11,38 +11,36 @@ public class Item : NetworkBehaviour
 
     private MeshRenderer meshRenderer;
 
-    public NetworkVariable<bool> isActive = new NetworkVariable<bool>(true, NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
+    public NetworkVariable<bool> isActive = new NetworkVariable<bool>(true, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
 
     void Start()
     {
         meshRenderer = GetComponent<MeshRenderer>();
     }
 
+    void OnDestroy()
+    {
+        isActive.Value = false;
+    }
+
     void Update()
     {
-
         if(!IsOwner)
         {
             return;
         }
 
-
-        // if(!isActive.Value && meshRenderer.enabled)
-        // {
-        //     meshRenderer.enabled = false;
-        //     Debug.Log("비활성화");
-        // }
-
-        if(!meshRenderer.enabled && isActive.Value)
+        if (!isActive.Value)
         {
-            isActive.Value = false;
-            Debug.Log("비활성화");
+            // 클라이언트에서 직접 값을 변경하는 대신 서버에 요청
+            UpdateIsActiveServerRpc(false);
         }
-
-        meshRenderer.enabled = isActive.Value;
-
-
     }
 
-    
+    [ServerRpc]
+    void UpdateIsActiveServerRpc(bool newValue)
+    {
+        isActive.Value = newValue;
+        Debug.Log("비활성화");
+    }
 }
