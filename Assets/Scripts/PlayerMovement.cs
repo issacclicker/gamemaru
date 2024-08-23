7f133fc2fdede6c4aea2c437ba782950eb3e3db0
gpdnjs70 <kshn0710@naver.com>
2024-08-23 13:55:05 +0900

    .

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -111,8 +111,8 @@ public class PlayerMovement : NetworkBehaviour
             //     Set_playerStateSyncServerRpc("Fox");
                 
             // }
-            playerState = "Tiger";  
-            Set_playerStateSyncServerRpc("Tiger");
+            playerState = "Fox";  
+            Set_playerStateSyncServerRpc("Fox");
         }
 
         // if(string.IsNullOrEmpty(playerState))
